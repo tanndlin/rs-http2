6258809ad49a7dee33f536fbe2a500587e55b43b
tanndlin <tanndlin@gmail.com>
2026-03-06 00:32:58 -0500

    add rest of header validation

--- src/http2/header_builder.rs
+++ src/http2/header_builder.rs
@@ -30,9 +30,22 @@ impl HeaderBuilder {
         // dbg!(&decoded_headers);
 
         let mut headers: HashMap<String, String> = HashMap::new();
+        let mut seen_regular_header = false;
         for (name, value) in decoded_headers {
             let name = String::from_utf8_lossy(&name);
             let value = String::from_utf8_lossy(&value);
+
+            // No duplicate pseudo headers allowed, and pseudo headers must come before regular headers
+            if name.starts_with(':') {
+                if headers.contains_key(&name.to_string()) || seen_regular_header {
+                    return Err(HTTP2Error::Stream(StreamError {
+                        stream_id,
+                        error_code: HTTP2ErrorCode::ProtocolError,
+                    }));
+                }
+            } else {
+                seen_regular_header = true;
+            }
 
             headers.insert(name.to_string(), value.to_string());
         }
@@ -50,9 +63,9 @@ impl HeaderBuilder {
 
         // Check for presence of pseudo headers and that they are valid
         if headers
-            .keys()
-            .filter(|h| h.starts_with(':'))
-            .any(|h| PsuedoHeader::from_str(h).is_err() || *h == PsuedoHeader::Status.to_string())
+            .iter()
+            .filter(|h| h.0.starts_with(':'))
+            .any(|(k, v)| !is_req_psuedo_header_valid(k, v))
         {
             return Err(HTTP2Error::Stream(StreamError {
                 stream_id,
@@ -60,18 +73,33 @@ impl HeaderBuilder {
             }));
         }
 
-        // Make sure all pseudo headers come before regular headers
-        let mut seen_regular_header = false;
-        for header_name in headers.keys() {
-            if header_name.starts_with(':') {
-                if seen_regular_header {
+        // Scheme and method must be present in requests
+        if !headers.contains_key(":scheme") || !headers.contains_key(":method") {
+            return Err(HTTP2Error::Stream(StreamError {
+                stream_id,
+                error_code: HTTP2ErrorCode::ProtocolError,
+            }));
+        }
+
+        // Must not contain connection-specific headers
+        for (key, value) in &headers {
+            match key.as_str() {
+                "connection" | "proxy-connection" | "keep-alive" | "transfer-encoding"
+                | "upgrade" => {
                     return Err(HTTP2Error::Stream(StreamError {
                         stream_id,
                         error_code: HTTP2ErrorCode::ProtocolError,
                     }));
                 }
-            } else {
-                seen_regular_header = true;
+                "te" => {
+                    if value.to_lowercase() != "trailers" {
+                        return Err(HTTP2Error::Stream(StreamError {
+                            stream_id,
+                            error_code: HTTP2ErrorCode::ProtocolError,
+                        }));
+                    }
+                }
+                _ => (),
             }
         }
 
@@ -85,4 +113,22 @@ impl HeaderBuilder {
     pub fn waiting_for_continuation(&self) -> bool {
         !self.data.is_empty()
     }
+}
+
+fn is_req_psuedo_header_valid(key: &str, value: &str) -> bool {
+    let Ok(header) = PsuedoHeader::from_str(key) else {
+        return false;
+    };
+
+    // Cant be :status
+    if let PsuedoHeader::Status = header {
+        return false;
+    }
+
+    // Value must not be empty
+    if value.is_empty() {
+        return false;
+    }
+
+    true
 }
